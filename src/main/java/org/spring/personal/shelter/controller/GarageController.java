ccdb1879c14d19c30d14b665f246882bc72bc642
Thomas <thomas.brun.perso@gmail.com>
2023-09-14 01:51:10 +0200

    myCar

--- src/main/java/org/spring/personal/shelter/controller/GarageController.java
+++ src/main/java/org/spring/personal/shelter/controller/GarageController.java
@@ -1,13 +1,15 @@
 package org.spring.personal.shelter.controller;
 
+import org.spring.personal.shelter.model.Car;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GarageController {
 
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "Hello world !";
+    @RequestMapping("/car")
+    public Car getCar(){
+        Car myCar = new Car("Picasso", "Citroen", 2004, Car.Color.RED);
+        return myCar;
     }
 }
